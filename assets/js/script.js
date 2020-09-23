cf5c0e03953294f4be53f89ff2e518b2f084a44a
Chris C <christopher.cruzcosa@gmail.com>
2020-09-22 17:47:35 -0700

    Added main weather functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 $(function () {
     var apiKey = "b3ae335adbb780337592ec0c99fc049f";
-    var city = "New York"
+    var city = "San Diego"
 
 
     function fetchWeatherForCity(city) {
+
         var queryUrl =
             "https://api.openweathermap.org/data/2.5/weather?q=" +
             city +
@@ -12,49 +13,56 @@ $(function () {
 
         $(".weather-card").addClass("card-loading");
 
-
         $.ajax({
             url: queryUrl,
             method: "GET",
-        }).then(function (data) {
-            console.log(data);
+        }).then(function (data1) {
+            console.log(data1);
 
-            $("#mainCityName").text(data.name + " (" + moment().format("M/D/YYYY") + ")");
+            $("#mainCityName").text(data1.name + " (" + moment().format("M/D/YYYY") + ")");
 
             var iconUrl =
-                "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
+                "https://openweathermap.org/img/wn/" + data1.weather[0].icon + "@2x.png";
 
             var iconImg = $("<img>").attr({
                 src: iconUrl,
-                alt: data.weather[0].description,
+                alt: data1.weather[0].description,
             });
 
-            var UV = ""
-
-            var lat = data.coord.lat;
-            var lon = data.coord.lon;
-
-            var queryUVUrl =
-                "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" +
-                lat +
-                "&lon=" +
-                lon +
-                "&appid=" +
-                apiKey;
-
+            
+            var lat = data1.coord.lat;
+            var lon = data1.coord.lon;
+            
+            
+            var queryUrlForecast =
+            "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+            lat +
+            "&lon=" +
+            lon +
+            "&units=imperial&exclude=minutely,hourly,alerts&appid=" +
+            apiKey;
+            
             $.ajax({
-                url: queryUVUrl,
+                url: queryUrlForecast,
                 method: "GET",
-            }).then(function (data) {
-                console.log(data);
-
-                $("#UVbtn").text(data[7].value);
+            }).then(function (data2) {
+                console.log(data2);
+                for (i = 1; i < 6; i++) {
+                    var day = moment().add(i,"days").format("M/D/YYYY")
+                    $("#forecastDate" + i).text("(" + day + ")");
+                    $("#forecastIcon" + i).attr("src","https://openweathermap.org/img/wn/" + data2.daily[i].weather[0].icon + "@2x.png");
+                    $("#forecastTemp" + i).text("Temp: " + data2.daily[i].temp.day + "°F");
+                    $("#forecastHum" + i).text("Humidity: " + data2.daily[i].humidity + "%");
+                }
+                
+                $("#UVbtn").text(data2.current.uvi);
             });
-
+            
+            
             $("#mainCityName").append(iconImg);
-            $("#tempLoc").text("Temperature: " + data.main.temp + "°F");
-            $("#windLoc").text("Wind Speed: " + data.wind.speed + " mph");
-            $("#humLoc").text("Humidity: " + data.main.humidity + " %");
+            $("#tempLoc").text("Temperature: " + data1.main.temp + "°F");
+            $("#windLoc").text("Wind Speed: " + data1.wind.speed + " mph");
+            $("#humLoc").text("Humidity: " + data1.main.humidity + " %");
             $(".weather-card").removeClass("card-loading");
         });
     }
